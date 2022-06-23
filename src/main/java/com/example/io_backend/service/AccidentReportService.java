a08751e6b9ac02df21edc3161d035d95d6bbcc2c
Bartłomiej Mazurski - s20479 <s20479@pjwstk.edu.pl>
2022-06-23 19:35:16 +0200

    Automatically assign report to dispositor (#41)
    * Now accident is automatically assigned to staff
    
    * Now accident is automatically assigned to staff
    
    * Take only staff currently on duty
    
    * build fix
    
    Co-authored-by: Bartosz Semeniuk <s20462@pjwstk.edu.pl>

--- src/main/java/com/example/io_backend/service/AccidentReportService.java
+++ src/main/java/com/example/io_backend/service/AccidentReportService.java
@@ -2,17 +2,25 @@ package com.example.io_backend.service;
 
 import com.example.io_backend.exception.NotFoundException;
 import com.example.io_backend.model.AccidentReport;
+import com.example.io_backend.model.Staff;
+import com.example.io_backend.model.enums.StaffType;
 import com.example.io_backend.repository.AccidentReportRepository;
+import com.example.io_backend.repository.DispositorDutyEntryRepository;
+import com.example.io_backend.repository.StaffRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 @RequiredArgsConstructor
 public class AccidentReportService {
 
     private final AccidentReportRepository accidentReportRepository;
+    private final StaffRepository staffRepository;
+    private final DispositorDutyEntryRepository dispositorDutyEntryRepository;
 
     public List<AccidentReport> getAccidentReports(){
         return accidentReportRepository.findAll();
@@ -23,6 +31,20 @@ public class AccidentReportService {
     }
 
     public AccidentReport addAccidentReport(AccidentReport accidentReport){
+        AtomicReference<Staff> least = null;
+        AtomicLong count = new AtomicLong(Long.MAX_VALUE);
+        staffRepository.getAllByStaffType(StaffType.DISPOSITOR).stream()
+                .filter(it -> dispositorDutyEntryRepository.getFirstByDutyEndIsNotNullAndStaffEquals(it) != null)
+                .forEach(it -> {
+                    long current = accidentReportRepository.countByStaff(it);
+                    if (current < count.get()) {
+                        count.set(current);
+                        least.set(it);
+                    }
+                });
+
+        accidentReport.setStaff(least.get());
+
         return accidentReportRepository.save(accidentReport);
     }
 
