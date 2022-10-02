a2d6c6d2b4b128bc13cebc3a69820978c2535570
Aleksandra Mira <s20383@pjwstk.edu.pl>
2022-10-02 21:50:06 +0200

    API for enums

---
+++ src/main/java/com/example/io_backend/controller/EnumController.java
@@ -0,0 +1,23 @@
+package com.example.io_backend.controller;
+
+import com.example.io_backend.model.enums.EmergencyType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController()
+@RequestMapping(path = "/enum")
+@RequiredArgsConstructor
+public class EnumController {
+	@GetMapping("/emergency_type")
+	public List<String> getEmergencyTypeList(){
+		return Stream.of(EmergencyType.values())
+				.map(Enum::name)
+				.collect(Collectors.toList());
+	}
+}
